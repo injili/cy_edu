0560d972547e94adfd5b1551e3e1fda18b35fd3f
injili <nyarekigospel@gmail.com>
2024-09-24 00:01:29 +0300

    this is for today

--- src/components/videos.jsx
+++ src/components/videos.jsx
@@ -1,30 +1,54 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
 
+const API_KEY = import.meta.env.VITE_STRAPI_API_KEY
+const API_URL = import.meta.env.VITE_STRAPI_API_URL
 
 export default function Videos() {
+    const {links, setLinks} = useState([]);
+
+    useEffect(() => {
+        axios.get(`${API_URL}/videos-case-studies`, {
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": `Bearer ${API_KEY}`
+            }
+        })
+        .then(response => {
+            const it = response.data.data.map(e => ({
+                id: e.id,
+                link: e.attributes.link,
+            }))
+            setLinks(it)
+        })
+        .catch(error => {
+            console.log("There was an error", error)
+        })
+    }, []);
+
     return (
         <div>
-            <div className="aspect-w-16 aspect-h-9 flex flex-wrap gap-4">
-                <iframe
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/BurTL9J2wZs?si=k3zm9nAUC9H0bIp7"
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                    allowfullscreen
-                >
-                </iframe>
-                <iframe
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/G4tfQI6hRNI?si=lc1_NXZL5ZJhNNld"
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                    allowfullscreen
-                >
-                </iframe>
-            </div>
+            {links.length > 0 ? (
+                 <div className="aspect-w-16 aspect-h-9 flex flex-wrap gap-4">
+                    {links
+                    .map((link, index) => (
+                    <iframe
+                        key={index}
+                        width="560"
+                        height="315"
+                        src={link}
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowfullscreen
+                    >
+                    </iframe>
+                    ))}
+                </div>
+            ) : (
+                <p>There are no Videos</p>
+            )}
+           
         </div>
     )
 }
