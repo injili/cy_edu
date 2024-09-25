de424c0f03011e2251046301e55287db3719e281
injili <nyarekigospel@gmail.com>
2024-09-25 15:13:44 +0300

    the shift to material UI

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -5,7 +5,7 @@ export default function Navigation() {
         <div className="flex justify-center">
             <div className="sticky top-0 w-[900px]">
                 <ul className="m-4 p-4 rounded-lg bg-secondprimary flex font-black gap-4 font-alata justify-center items-center text-firstprimary">
-                <Link to="/"><li className="hover:text-thirdprimary">DASHBOARD</li></Link>
+                    <Link to="/"><li className="hover:text-thirdprimary">DASHBOARD</li></Link>
                     <Link to="/training"><li className="hover:text-thirdprimary">TRAINING</li></Link>
                     <Link to="/interactive"><li className="hover:text-thirdprimary">VIDEOS AND CASE STUDIES</li></Link>
                     <Link to="/visualization"><li className="hover:text-thirdprimary">VISUALIZATION</li></Link>
